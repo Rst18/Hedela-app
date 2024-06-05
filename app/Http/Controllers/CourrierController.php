1b3c5f56bfee4badb327670cadaa980a67535ced
Rst18 <kaserekamwiraros@gmail.com>
2024-06-05 07:47:44 -0700

    [NEW] Create courrier Model, controller, migration,requests,...

---
+++ app/Http/Controllers/CourrierController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Courrier;
+use App\Http\Requests\StoreCourrierRequest;
+use App\Http\Requests\UpdateCourrierRequest;
+
+class CourrierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCourrierRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Courrier $courrier)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Courrier $courrier)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCourrierRequest $request, Courrier $courrier)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Courrier $courrier)
+    {
+        //
+    }
+}
