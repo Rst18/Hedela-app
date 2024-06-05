f78581fee32e455a10efb9c906412d162acf1f68
Rst18 <kaserekamwiraros@gmail.com>
2024-06-05 06:49:20 -0700

    [ADD] crud service

---
+++ app/Models/Service.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Role;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Service extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['code','name'];
+
+    /**
+     * The roles that belong to the Service
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
+}
