1bdddc6f077f3f8da05910aa753de76215e1772d
Rst18 <kaserekamwiraros@gmail.com>
2024-06-11 08:46:15 -0700

    [NEW] Enregistrement de l'audience

---
+++ app/Http/Requests/StoreAudienceRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAudienceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+              'name'=>'required',
+              'email'=>'required|email',
+              'phone'=>'required',
+              'motif'=>'required',
+              'user_id'=>'required|exists:users,id',
+              'user_requested'=>'required|exists:users,id',
+              'date_proposition'=>'date',
+              'piece'=>'required',
+              'status'=>'integer'
+        ];
+
+      
+    }
+
+    public function PrepareForValidation(){
+        
+        $this->mergeIfMissing([
+            
+            'user_id'=>Auth::user()->id
+        ]);
+    }
+}
---
+++ database/migrations/2024_06_11_152924_create_audiences_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('audiences', function (Blueprint $table) {
+            
+            $table->id();
+            $table->mediumText('name');
+            $table->mediumText('fonction');
+            $table->string('phone', 100);
+            $table->string('piece', 100);
+            $table->string('email', 100)->nullable();
+            $table->text('motif');
+            $table->date('date_proposition');
+            $table->unsignedBigInteger('user_requested');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->integer('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('audiences');
+    }
+};
