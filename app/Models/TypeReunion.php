e0d3b16c1501e3b9a1225741e0fcbcf9f978e5c2
Rst18 <kaserekamwiraros@gmail.com>
2024-07-09 04:25:14 -0700

    [NEW] Type de reunion

---
+++ app/Models/TypeReunion.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Reunion;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class TypeReunion extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name','description'];
+
+    /**
+     * Get all of the reunions for the TypeReunion
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function reunions(): HasMany
+    {
+        return $this->hasMany(Reunion::class);
+    }
+}
