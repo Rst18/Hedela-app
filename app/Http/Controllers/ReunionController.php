c004f3cc1df458d803c5d490608de69cb6bf4d31
Rst18 <kaserekamwiraros@gmail.com>
2024-07-12 07:53:23 -0700

    [FIX] Save reunion fixed

--- app/Http/Controllers/ReunionController.php
+++ app/Http/Controllers/ReunionController.php
@@ -56,7 +56,7 @@ class ReunionController extends Controller
      */
     public function store(StoreReunionRequest $request)
     {
-        //return $request;
+        
       
         try {
             
@@ -82,12 +82,12 @@ class ReunionController extends Controller
                     $ordre = OrdreJour::create([
                         
                         'name'=>$request->ordreJour[$i]['name'],
-                        'description'=>$request->ordreJour[$i]['description'],
+                        'description'=>isset($request->ordreJour[$i]['description']) ? $request->ordreJour[$i]['description'] : '' ,
                         'reunion_id'=>$reunion->id
                     
                     ]);
 
-                    if (count($request->ordreJour[$i]['annexes']) > 0 ) {
+                    if ( isset($request->ordreJour[$i]['annexes']) ) {
 
                         for ($a=0; $a < count($request->ordreJour[$i]['annexes']) ; $a++) { 
 
@@ -116,8 +116,8 @@ class ReunionController extends Controller
         } catch (\Throwable $th) {
             //throw $th;
             DB::rollBack();
-            return ['type'=>'error','message'=>'Echec d\'Enregistrement','errorMessage'=>$th];
-       }
+             return ['type'=>'error','message'=>'Echec d\'Enregistrement','errorMessage'=>$th];
+        }
     }
 
     /**
